6efbce7764a26b4189209b5bb154ce6c92190ac7
Justine Kenji Dela Cruz <kenjidckenji@gmail.com>
2024-08-13 13:26:10 -0400

    Initial Commit

---
+++ PhotoForNames/PhotoForNamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoForNamesApp.swift
+//  PhotoForNames
+//
+//  Created by Kenji Dela Cruz on 8/13/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoForNamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
